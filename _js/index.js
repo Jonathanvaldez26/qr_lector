fb4149f717b039b8103f6c200e8863bb0f8f6389
Gerson Velasco <gvelassco@gmail.com>
2022-04-14 09:58:23 -0500

    hola

--- _js/index.js
+++ _js/index.js
@@ -26,10 +26,13 @@ jQuery(document).ready(function(){
   });
   
   Instascan.Camera.getCameras().then(function (cameras) {
-      if (cameras.length > 0) {
+      if (cameras.length == 1) {
+        scanner.start(cameras[0]);
+        console.log(cameras[0].name);
+      } else if(cameras.length >= 2){
         scanner.start(cameras[1]);
         console.log(cameras[1].name);
-      } else {
+      }else {
         console.error('No cameras found.');
       }
     }).catch(function (e) {
